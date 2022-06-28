02cf3157c5a351aa3eb23a509a3ad5e0e5b7cbe5
Hồ Minh Cảnh <minhcanhbinhdai@gmail.com>
2022-06-29 00:39:33 +0700

    git commit

---
+++ front/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using front.Views.Products;
+using shared;
+using front.Views.Products;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace front.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: /<controller>/
+       
+        public IActionResult Index()
+        {
+            //var dataset =await GetProducts();
+            return View(); //dataset);
+        }
+        
+    }
+}
+
---
+++ front/Views/Products/ProductsDetails.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace front.Views.Products
+{
+	public class ProductsDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
