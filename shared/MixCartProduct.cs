8d0f24ebd609bbb61e6adfc3d8c399fb32664a25
Hồ Minh Cảnh <minhcanhbinhdai@gmail.com>
2022-07-12 22:18:50 +0700

    git add
    Signed-off-by: Hồ Minh Cảnh <minhcanhbinhdai@gmail.com>

---
+++ shared/MixCartProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Projects.Shared;
+
+namespace Shared
+{
+	public class MixCartProduct
+	{
+		public CartItem cartItem { get; set; }
+        public Product product { get; set; }
+		public int sum { get; set; }
+		public MixCartProduct()
+		{
+		}
+        public MixCartProduct(CartItem cartItem,Product product)
+        {
+			this.cartItem = cartItem;
+			this.product = product;
+            this.sum = (int)(cartItem.Quantity * product.Variants[0].Price);
+        }
+      
+	}
+}
+
